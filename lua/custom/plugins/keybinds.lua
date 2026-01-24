9e745b80f82ce584d2c120d84d8c7c10dc09a9f6
Peter C <chevezpeter@gmail.com>
2026-01-24 12:54:55 -0800

    Adding MacOS edits.

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,20 +1,15 @@
 -- Explorer with space - pv
 vim.keymap.set('n', '-', vim.cmd.Ex)
 
--- Use PowerShell as the default shell
-vim.opt.shell = 'pwsh' -- or "powershell" if you use Windows PowerShell
-vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
-vim.opt.shellquote = ''
-vim.opt.shellxquote = ''
---
--- Lil something for deleting current buffer with Alt-k
-vim.api.nvim_set_keymap('n', '<M-k>', ':bd<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<M-K>', ':qa!<CR>', { noremap = true, silent = true })
--- for saving all files.
-vim.api.nvim_set_keymap('n', '<M-w>', ':wa<CR>', { noremap = true, silent = true })
+local map = function(mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
+end
 
-vim.api.nvim_set_keymap('n', '<leader>bl', ':ls<CR>', { noremap = true, silent = true })
+-- Buffer management
+map('n', '<leader>k', ':bd<CR>') -- delete current buffer
+map('n', '<leader>K', ':qa!<CR>') -- quit all (force)
+map('n', '<leader>w', ':wa<CR>') -- write all files
+map('n', '<leader>bl', ':ls<CR>') -- list buffers
+
 -- vim: ts=2 sts=2 sw=2 et
---
---
 return {}
